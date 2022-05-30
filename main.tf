5f6dfe1131a8f541d56583d293cdc49ee70a65f5
Rajesh-git-web <79069346+Rajesh-git-web@users.noreply.github.com>
2022-05-30 15:57:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,14 +85,14 @@ resource "aws_security_group" "sg_22" {
 
 
 
-resource "aws_instance" "testInstance" {
-  ami           = "${var.instance_ami}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  key_name = "key_new_aws"
-  tags = {
-    Environment = "${var.environment_tag}"
-    Name = "${var.environment_tag} Ec2 Terraform"
-  }
-}
+# resource "aws_instance" "testInstance" {
+#   ami           = "${var.instance_ami}"
+#   instance_type = "${var.instance_type}"
+#   subnet_id = "${aws_subnet.subnet_public.id}"
+#   vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
+#   key_name = "key_new_aws"
+#   tags = {
+#     Environment = "${var.environment_tag}"
+#     Name = "${var.environment_tag} Ec2 Terraform"
+#   }
+# }
